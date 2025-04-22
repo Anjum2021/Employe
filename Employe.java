93d2636fd6fecd814e6c7fff68b097c4fb6f9e90
Anjum2021 <anjumrnadaf086@gmail.com>
2025-04-22 14:10:36 +0530

    save the file

---
+++ Employe.java
@@ -0,0 +1,16 @@
+public class Employe {
+    public static void main(String[] args) {
+        String name="Anjum";
+        int id=076;
+        String clg="kle";
+        String course="Devops";
+
+        System.out.println("-------Student Details-----");
+        System.out.println("Student name:"+name);
+        System.out.println("Student name:"+id);
+        System.out.println("Student name:"+clg);
+        System.out.println("Student name:"+course);
+
+    }
+
+}
